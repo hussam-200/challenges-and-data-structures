f39517b5e32ed85f34d7236c9a2a79ffb2334020
hussam-200 <hussamibrahim434@gmail.com>
2025-07-19 14:51:02 +0300

    DeleteMiddleElement

--- DataStructures/StackAndQueue/StackAndQueue-Implementation/app.js
+++ DataStructures/StackAndQueue/StackAndQueue-Implementation/app.js
@@ -1,8 +1,10 @@
 const Stack = require("./Stack");
 const Queue=require("./Queue");
+const StackWithDeleteMiddle=require("../DeleteMiddleELement/DeleteMiddleElement")
 
 const stack = new Stack();
 const queue=new Queue();
+const stackDelete=new StackWithDeleteMiddle();
 
 stack.push(2);
 stack.push(4);
@@ -26,8 +28,6 @@ queue.dequeue();
 queue.printq();
 
 
-
-
 console.log(stack.peek());
 console.log(queue.peek());
 
@@ -36,9 +36,21 @@ console.log(queue.peek());
 console.log(stack.isEmpty());
 console.log(queue.isEmpty());
 
+stackDelete.push(2);
+stackDelete.push(4);
+stackDelete.push(6);
+stackDelete.push(8);
+console.log("this stack before delete middle (odd number)");
+stackDelete.print();
+stackDelete.deleteMiddle();
+console.log("this stack after delete middle");
+stackDelete.print();
 
-
-
-
-
+stackDelete.push(10);
+stackDelete.push(12);
+console.log("this stack before delete middle (even number)");
+stackDelete.print();
+stackDelete.deleteMiddle();
+console.log("this stack after delete middle");
+stackDelete.print();
 
