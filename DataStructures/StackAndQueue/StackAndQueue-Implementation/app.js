e6f75e38a5b9296d95cb8bcc782057efa84402e0
hussam-200 <hussamibrahim434@gmail.com>
2025-07-27 15:28:35 +0300

    MinStack

--- DataStructures/StackAndQueue/StackAndQueue-Implementation/app.js
+++ DataStructures/StackAndQueue/StackAndQueue-Implementation/app.js
@@ -62,17 +62,20 @@ MinStack.push(7);
 MinStack.push(12);
 MinStack.push(3);
 console.log("stack list:");
-MinStack.print();
+
+console.log(MinStack.printStack());
 
 
-console.log("Min Number in stack list:",MinStack.GetMin());
+
+console.log("Min Number in stack list:",MinStack.getMin());
 
 console.log("pop number of stack list:",MinStack.pop())
-console.log("Min Number in stack list after poped :",MinStack.GetMin());
-console.log("Peek Number in stack list:",MinStack.peek());
+console.log("Min Number in stack list after poped :",MinStack.getMin());
 MinStack.push(2);
-MinStack.print();
-console.log("Min Number in stack list after push the new element (2):",MinStack.GetMin());
+
+console.log(MinStack.printStack());
+
+console.log("Min Number in stack list after push the new element (2):",MinStack.getMin());
 
 
 console.log("check if the stack list empty:",MinStack.isEmpty());
